899881028e6be7198933063fe4a4ca1f71b4a2f4
abhishekmishravlog <abhishek.mish2002@gmail.com>
2023-06-25 16:06:45 +0530

    header page added

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -2,11 +2,12 @@ import React from "react";
 import { useSelector } from 'react-redux'
 import { toast } from "react-toastify";
 import Spinner from "../components/shared/Spinner";
+import Layout from "../components/shared/Layout/Layout";
 
 const Homepage = () => {
     const { loading, error } = useSelector((state) => state.auth);
     return (
-        <>
+        <Layout>
             {/* {error && <span>{toast(error)}</span>} */}
             {error && <span>{alert(error)}</span>}
             {loading ? (
@@ -16,7 +17,7 @@ const Homepage = () => {
                     <h1>Home Page</h1>
                 </div>
             )}
-        </>
+        </Layout>
     )
 }
 
