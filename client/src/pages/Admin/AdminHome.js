70118de292aac729b728c0d7a47b8aecf808be53
Code With Anurag <anurag.am2003@gmail.com>
2023-06-28 00:59:33 +0530

    admin homepage created

--- client/src/pages/Admin/AdminHome.js
+++ client/src/pages/Admin/AdminHome.js
@@ -1,12 +1,21 @@
-import React from 'react'
-import Layout from '../../components/shared/Layout/Layout'
+import React from "react";
+import Layout from "../../components/shared/Layout/Layout";
+import { useSelector } from "react-redux";
 
 const AdminHome = () => {
+    const { user } = useSelector((state) => state.auth);
     return (
         <Layout>
-            <h1>Admin Home</h1>
+            <div className="container">
+                <div className="d-felx flex-column mt-4">
+                    <h1>
+                        Welcome Admin <i className="text-success">{user?.name}</i>
+                    </h1>
+                    <h3>Manage Blood Bank App </h3>
+                </div>
+            </div>
         </Layout>
-    )
-}
+    );
+};
 
-export default AdminHome
+export default AdminHome;
