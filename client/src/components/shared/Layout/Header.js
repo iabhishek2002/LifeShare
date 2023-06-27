77978d2d21a24639907c18fdb7686f66e4209170
Code With Anurag <anurag.am2003@gmail.com>
2023-06-27 23:07:31 +0530

    blood calculations

--- client/src/components/shared/Layout/Header.js
+++ client/src/components/shared/Layout/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { BiDonateBlood, BiUserCircle } from "react-icons/bi";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 const Header = () => {
     const { user } = useSelector((state) => state.auth);
     const navigate = useNavigate();
+    const location = useLocation();
 
     // logout handler
     const handleLogout = () => {
@@ -29,6 +30,21 @@ const Header = () => {
                                 <span className="badge bg-secondary">{user?.role}</span>
                             </p>
                         </li>
+                        {
+                            location.pathname === '/' ? (
+                                <li className="nav-item mx-3">
+                                    <Link to="/analytics" className="nav-link">
+                                        Analytics
+                                    </Link>
+                                </li>
+                            ) : (
+                                <li className="nav-item mx-3">
+                                    <Link to="/" className="nav-link">
+                                        Home
+                                    </Link>
+                                </li>
+                            )
+                        }
                         <li className="nav-item mx-3">
                             <button className="btn btn-danger" onClick={handleLogout}>
                                 Logout
