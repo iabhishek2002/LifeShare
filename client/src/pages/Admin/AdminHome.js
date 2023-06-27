1765ba2ecdee94197a02d349687e0a6d21781003
Code With Anurag <anurag.am2003@gmail.com>
2023-06-28 00:53:52 +0530

    admin layout and routes created

---
+++ client/src/pages/Admin/AdminHome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/shared/Layout/Layout'
+
+const AdminHome = () => {
+    return (
+        <Layout>
+            <h1>Admin Home</h1>
+        </Layout>
+    )
+}
+
+export default AdminHome
---
+++ client/src/pages/Admin/DonarList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/shared/Layout/Layout'
+
+const DonarList = () => {
+    return (
+        <Layout>
+            <h1>Donor List</h1>
+        </Layout>
+    )
+}
+
+export default DonarList
