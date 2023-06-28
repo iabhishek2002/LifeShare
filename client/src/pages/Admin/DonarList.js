c2257c428bb999933e1d1be1d712102ea800d51c
Code With Anurag <anurag.am2003@gmail.com>
2023-06-28 20:29:36 +0530

    donor list in admin page

--- client/src/pages/Admin/DonarList.js
+++ client/src/pages/Admin/DonarList.js
@@ -1,12 +1,51 @@
-import React from 'react'
-import Layout from '../../components/shared/Layout/Layout'
+import React, { useEffect, useState } from "react";
+import Layout from "./../../components/shared/Layout/Layout";
+import moment from "moment";
+import API from "../../services/API";
 
 const DonarList = () => {
+    const [data, setData] = useState([]);
+    //find donar records
+    const getDonars = async () => {
+        try {
+            const { data } = await API.get("/admin/donar-list");
+            //   console.log(data);
+            if (data?.success) {
+                setData(data?.donarData);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getDonars();
+    }, []);
+
     return (
         <Layout>
-            <h1>Donor List</h1>
+            <table className="table ">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data?.map((record) => (
+                        <tr key={record._id}>
+                            <td>{record.name || record.organisationName + " (ORG)"}</td>
+                            <td>{record.email}</td>
+                            <td>{record.phone}</td>
+                            <td>{moment(record.createdAt).format("DD/MM/YYYY hh:mm A")}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </Layout>
-    )
-}
+    );
+};
 
-export default DonarList
+export default DonarList;
